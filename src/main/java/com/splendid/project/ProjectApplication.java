421640edcd341b74a2e388bc6633c4febcb117cc
limwinjoon <fkxl9511@gmail.com>
2024-08-22 14:09:10 +0900

    setting

--- src/main/java/com/splendid/project/ProjectApplication.java
+++ src/main/java/com/splendid/project/ProjectApplication.java
@@ -1,13 +1,13 @@
-package com.swinnus.reefer.pofol;
+package com.splendid.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PofolApplication {
+public class ProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PofolApplication.class, args);
+        SpringApplication.run(ProjectApplication.class, args);
     }
 
 }
