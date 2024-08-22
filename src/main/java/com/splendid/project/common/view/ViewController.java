ec0ffaaca8af8c6dc10fefbd9543988a748cdcca
limwinjoon <fkxl9511@gmail.com>
2024-08-22 17:05:11 +0900

    setting

---
+++ src/main/java/com/splendid/project/common/view/ViewController.java
@@ -0,0 +1,10 @@
+package com.splendid.project.common.view;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class ViewController {
+    // view
+}
