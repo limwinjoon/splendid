945697ebc116d9fb025db4bdbe165b7f25c268a6
limwinjoon <fkxl9511@gmail.com>
2024-08-22 13:54:36 +0900

    spring

---
+++ src/main/java/com/swinnus/reefer/pofol/PofolApplication.java
@@ -0,0 +1,13 @@
+package com.swinnus.reefer.pofol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PofolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PofolApplication.class, args);
+    }
+
+}
