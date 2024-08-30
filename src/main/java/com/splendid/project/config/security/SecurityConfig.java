b8e83cfb8af096e7d90d859d5000d5de9c0c4022
limwinjoon <fkxl9511@gmail.com>
2024-08-30 16:50:11 +0900

    security setting

--- src/main/java/com/splendid/project/config/security/SecurityConfig.java
+++ src/main/java/com/splendid/project/config/security/SecurityConfig.java
@@ -3,9 +3,14 @@ package com.splendid.project.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -17,4 +22,18 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().requestMatchers("");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests((authz) -> authz
+                .anyRequest().authenticated()
+        )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
 }
